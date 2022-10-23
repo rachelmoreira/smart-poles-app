8308fcce8613b10ec79093e2e2fe9bfc34ee2c4d
rachelmoreira <rachelmoreira00@gmail.com>
2022-10-23 11:35:41 -0300

    Routes

--- src/routes/LoginRoute.js
+++ src/routes/LoginRoute.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
-import { Text } from "react-native";
 
 import Login from "../screens/Login";
 
@@ -10,6 +9,20 @@ import AppRoutes from "./AppRoutes";
 const Stack = createNativeStackNavigator();
 
 export default function LoginRoute() {
+  function getHeaderTitle(params) {
+    let condominium = "";
+    const base = [
+      { label: "Condomínio Wall Street", value: "1" },
+      { label: "Condomínio Fulano de Tal", value: "2" },
+    ];
+    base.map((element) => {
+      if (element.value === params) {
+        return (condominium = element.label);
+      }
+    });
+    return condominium;
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -33,80 +46,11 @@ export default function LoginRoute() {
         <Stack.Screen
           name="MainApp"
           component={AppRoutes}
-          options={{ title: "Condomínio Wall Street", headerShadowVisible: false }}
+          options={({ route }) => ({
+            title: getHeaderTitle(route.params),
+          })}
         />
       </Stack.Navigator>
     </NavigationContainer>
-    // <Text>text</Text>
   );
 }
-
-// import React from "react";
-
-// import { NavigationContainer } from "@react-navigation/native";
-// import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-// import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
-
-// import Home from "../screens/Home";
-// import Temperature from "../screens/Temperature";
-// import Energy from "../screens/Energy";
-// import Sound from "../screens/Sound";
-// import Login from "../screens/Login";
-
-// // const IconHome =
-
-// const Tab = createBottomTabNavigator();
-
-// export default function LoginRoute() {
-//   return (
-//     // <NavigationContainer>
-//       <Tab.Navigator
-//         screenOptions={({ route }) => ({
-//           tabBarIcon: ({ focused, color, size }) => {
-//             let iconName;
-
-//             // if (route.name === "Home") {
-//             //   iconName = "home";
-//             //   // focused
-//             //   //   ? 'home'
-//             //   //   : 'ios-information-circle-outline';
-//             // } else if (route.name === "Temperatura") {
-//             //   iconName = "temperature-high";
-//             // }
-
-//             switch (route.name) {
-//               case "Home":
-//                 iconName = "home";
-//                 break;
-//               case "Temperatura umidade":
-//                 iconName = "temperature-high";
-//                 break;
-//               case "Sons e ruídos":
-//                 iconName = "volume-up";
-//                 break;
-//               case "Consumo de energia":
-//                 iconName = "bolt";
-//                 break;
-//             }
-
-//             // You can return any component that you like here!
-//             return <FontAwesome5 name={iconName} size={size} color={color} />;
-//           },
-//           headerShown: false,
-//           tabBarActiveTintColor: "#4d6979",
-//           tabBarInactiveTintColor: "#C7C7C7",
-//           labelStyle: {
-//             width: '100%',
-//             marginTop: 30,
-//             paddingTop: 21
-//           }
-//         })}
-//       >
-//         <Tab.Screen name="Home" component={Home} />
-//         <Tab.Screen name="Temperatura umidade" component={Temperature} />
-//         <Tab.Screen name="Sons e ruídos" component={Sound} />
-//         <Tab.Screen name="Consumo de energia" component={Energy}/>
-//       </Tab.Navigator>
-//     // </NavigationContainer>
-//   );
-// }
